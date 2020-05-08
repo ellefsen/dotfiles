d6821f87e5705ad5eafe71b5ab13f2b25e929fc4
Kim Ellefsen <kim@ellefsen.me>
2020-05-08 23:06:14 +0200

    WIP

--- install-codespaces.sh
+++ install-codespaces.sh
@@ -2,11 +2,11 @@
 
 echo "Setting up your environment..."
 
+LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
+
 apt-get update \
     && apt-get install -y curl zip unzip git software-properties-common wget \
-    && add-apt-repository -y ppa:ondrej/php \
-    && apt-get update \
-    && apt-get install -y php7.3-cli php7.3-gd php7.3-mysql \
+    && apt-get install -y php7.3 php7.3-cli php7.3-common php7.3-gd php7.3-mysql \
         php7.3-pgsql php7.3-imap php-memcached php7.3-mbstring php7.3-xml php7.3-curl \
         php7.3-bcmath libgmp-dev php7.3-gmp php7.3-zip php7.3-redis nano \
     && php -r "readfile('http://getcomposer.org/installer');" | php -- --install-dir=/usr/bin/ --filename=composer \
@@ -27,9 +27,3 @@ ln -s $HOME/.dotfiles/.php_cs $HOME/.php_cs
 curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
 # TODO: Add .zshrc
-
-# Install Composer
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
